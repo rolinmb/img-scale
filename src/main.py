9833f3c1d3e6b3829bde249af81c6898f19a51d2
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-10-30 23:14:57 -0500

    oops pls dont look charli

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ from PIL import Image
 
 def scale_img(in_path, out_path, dims):
     with Image.open(in_path) as img:
-        img_resized = img.resize(dims, Image.ANTIALIAS)
+        img_resized = img.resize(dims)
         img_resized.save(out_path, format='png')
 
 if __name__ == '__main__':
-    inpath = 'img-in/'
-    outath = 'img-out/out.png'
+    inpath = 'img-in/trial6_09262023_8.png'
+    outpath = 'img-out/out.png'
     outdims = (400, 400)
     scale_img(inpath, outpath, outdims)
